756a4e5c57fe76e871deb48aa14cb6d1b13d74b3
Jean personal <jeanschoen@hotmail.com>
2024-07-26 10:39:00 +0200

    added randomly place black square not overlapping

--- main.py
+++ main.py
@@ -12,9 +12,31 @@ def add_randomly_placed_squares(random_images, image_shape, square_size = 50):
             random_position_white_square_y = np.random.randint(half_square, high=image_shape[0]-half_square, dtype=int)
             random_position_white_square_x = np.random.randint(half_square, high=image_shape[1]-half_square, dtype=int)
 
+           # Then we define an array of possible x and y positions for the black square by taking away overlapping 
+            possible_positions = [
+                (x, y) for x in range(half_square, image_shape[1] - half_square) # also make sure in
+                for y in range(half_square, image_shape[0] - half_square)
+                if not (
+                    (x + half_square > random_position_white_square_x - half_square) and
+                    (x - half_square < random_position_white_square_x + half_square) and
+                    (y + half_square > random_position_white_square_y - half_square) and
+                    (y - half_square < random_position_white_square_y + half_square)
+                )
+            ]
+
+            # Choose a random position from the possible positions
+            random_position_black_square_x, random_position_black_square_y = possible_positions[np.random.randint(len(possible_positions))]
+
+             # Place the black square at the random position
             image[random_position_white_square_y - half_square : random_position_white_square_y + half_square,
                 random_position_white_square_x  - half_square : random_position_white_square_x  + half_square] = [255, 255, 255]
+            
+             # Place the black square at the random position
+            image[random_position_black_square_y - half_square:random_position_black_square_y + half_square,
+                  random_position_black_square_x - half_square:random_position_black_square_x + half_square] = [0, 0, 0]
+
             processed_batch.append(image)
+            
         batches.append(np.array(processed_batch))
     return batches
 
